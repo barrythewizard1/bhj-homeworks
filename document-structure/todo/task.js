3ebc7c34107e5fed3a988a382ac0b3840f721398
Barry <barryarr3@gmail.com>
2023-12-12 21:23:22 +0300

    2nd fix

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,37 +4,32 @@ const inputTask = document.getElementById('new-task');
 const addTask = (taskText) => {
     const taskElement = document.createElement('div');
     taskElement.classList.add('task');
-    
+
     const taskTitle = document.createElement('div');
     taskTitle.classList.add('task__title');
     taskTitle.textContent = taskText;
-    
+
     const removeButton = document.createElement('a');
     removeButton.href = "#";
     removeButton.classList.add('task__remove');
     removeButton.innerHTML = '&times;';
-    
+
     removeButton.addEventListener('click', () => {
-      taskList.removeChild(taskElement);
+        taskList.removeChild(taskElement);
     });
-    
+
     taskElement.appendChild(taskTitle);
     taskElement.appendChild(removeButton);
-    
+
     taskList.appendChild(taskElement);
 };
 
 const addButton = document.getElementById('add-button');
-addButton.addEventListener('click', () => {
-    if (inputTask.value.trim() !== '') {
-        addTask(inputTask.value);
-        inputTask.value = ''; 
-    }
-});
-
-inputTask.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter' && inputTask.value.trim() !== '') {
-        addTask(inputTask.value);
-        inputTask.value = ''; 
-      }
-  });
+if (addButton) {
+    addButton.addEventListener('click', () => {
+        if (inputTask.value.trim() !== '') {
+            addTask(inputTask.value);
+            inputTask.value = '';
+        }
+    });
+}
